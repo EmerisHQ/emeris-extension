d64552596cabb153ef33c3bcc828426c8f991409
Fabian Weber <fabian.weber@tendermint.com>
2022-04-09 18:23:32 +0800

    don't shut down the window on opening

--- e2e/welcome-page.spec.ts
+++ e2e/welcome-page.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from './extension-setup';
 test.describe('Welcome page', () => {
     test('Loader shows', async ({ page }) => {
         await page.goto(
-            'chrome-extension://majdpimiaehlcejkaoeedbhdiklflkng/popup.html'
+            `chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true`
         )
         await page.waitForSelector('.ephemeris');
     });
