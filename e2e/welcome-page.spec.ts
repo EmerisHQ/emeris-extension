37338eb90429186bd3698168329be72130543da3
Fabian Weber <fabian.weber@tendermint.com>
2022-04-09 17:54:27 +0800

    add rando pem key

--- e2e/welcome-page.spec.ts
+++ e2e/welcome-page.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from './extension-setup';
 test.describe('Welcome page', () => {
     test('Loader shows', async ({ page }) => {
         await page.goto(
-            'chrome-extension://bfgnlimijmfdeglfpcmdlcebeifaikng/popup.html'
+            'chrome-extension://majdpimiaehlcejkaoeedbhdiklflkng/popup.html'
         )
         await page.waitForSelector('.ephemeris');
     });
