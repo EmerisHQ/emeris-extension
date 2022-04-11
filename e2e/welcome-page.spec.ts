a229ec885f18c7180136f70695cb53b2b94e7252
Fabian Weber <fabian.weber@tendermint.com>
2022-04-11 15:54:08 +0800

    fix testing url

--- e2e/welcome-page.spec.ts
+++ e2e/welcome-page.spec.ts
@@ -2,7 +2,7 @@ import { test } from './extension-setup';
 
 test.describe('Welcome page', () => {
   test('Loader shows', async ({ page }) => {
-    await page.goto(`www.google.de`);
+    await page.goto(`https://www.google.de`);
     await page.waitForSelector('.ephemeris');
   });
 });
