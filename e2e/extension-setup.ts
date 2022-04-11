f7cb100c28833ff88220e33e028643c615a63cd6
Fabian Weber <fabian.weber@tendermint.com>
2022-04-11 20:51:42 +0800

    add sandbox flags

--- e2e/extension-setup.ts
+++ e2e/extension-setup.ts
@@ -1,28 +1,27 @@
-const { test: base, chromium, webkit } = require('@playwright/test')
-const path = require('path')
+import { chromium, test as base, webkit } from '@playwright/test';
+import path from 'path';
 
-const extensionPath = path.join(__dirname, '../dist') // make sure this is correct
+const extensionPath = path.join(__dirname, '../dist'); // make sure this is correct
 
 export const test = base.extend({
-    context: async ({ browserName }, use) => {
-        const browserTypes = { chromium, webkit }
-        const launchOptions = {
-            devtools: true,
-            headless: false,
-            viewport: {
-                width: 1920,
-                height: 1080
-            },
-            args: [
-                `--disable-extensions-except=${extensionPath}`,
-                `--load-extension=${extensionPath}`
-            ]
-        }
-        const context = await browserTypes[browserName].launchPersistentContext(
-            '',
-            launchOptions
-        )
-        await use(context)
-        await context.close()
-    }
-})
+  context: async ({ browserName }, use) => {
+    const browserTypes = { chromium, webkit };
+    const launchOptions = {
+      devtools: true,
+      headless: false,
+      viewport: {
+        width: 1920,
+        height: 1080,
+      },
+      args: [
+        `--no-sandbox`,
+        `--disable-setuid-sandbox`,
+        `--disable-extensions-except=${extensionPath}`,
+        `--load-extension=${extensionPath}`,
+      ],
+    };
+    const context = await browserTypes[browserName].launchPersistentContext('', launchOptions);
+    await use(context);
+    await context.close();
+  },
+});
