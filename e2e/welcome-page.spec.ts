f8aeadebd7312a5c549a3f0d2e8eb42baff5fb07
Fabian <frznhope@gmail.com>
2022-04-15 18:45:31 +0700

    chore: account create e2e (#30)
    Co-authored-by: Fabian Weber <fabian.weber@tendermint.com>

--- e2e/welcome-page.spec.ts
+++ e2e/welcome-page.spec.ts
@@ -1,8 +1,16 @@
+import { expect } from '@playwright/test';
+
 import { test } from './extension-setup';
 
 test.describe('Welcome page', () => {
   test('Loader shows', async ({ page }) => {
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true`);
     await page.waitForSelector('.ephemeris');
+  });
+  test('Welcome shows', async ({ page }) => {
+    await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true`);
+    await expect(page.locator('text=Create Account >> visible=true')).toBeVisible();
+    await expect(page.locator('text=Import Account >> visible=true')).toBeVisible();
+    await expect(page.locator('text=Import Ledger >> visible=true')).toBeVisible();
   });
 });
