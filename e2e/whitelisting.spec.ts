d4497918f9ae881c1793800d681d04833bce86a4
Fabian <frznhope@gmail.com>
2022-04-18 19:03:44 +0700

    Chore/e2e sign (#45)
    Co-authored-by: Fabian Weber <fabian.weber@tendermint.com>

--- e2e/whitelisting.spec.ts
+++ e2e/whitelisting.spec.ts
@@ -1,10 +1,12 @@
 import { expect } from '@playwright/test';
 
 import { test } from './extension-setup';
+import { emerisLoaded, enableWebsite } from './helpers';
 
 test.describe('Whitelisting', () => {
   test('Request page whitelisting', async ({ page, context }) => {
-    await page.goto(`https://app.emeris.com`);
+    await page.goto(`https://www.google.com/`);
+    await emerisLoaded(page);
 
     // negative test
     expect(
@@ -13,15 +15,7 @@ test.describe('Whitelisting', () => {
       }),
     ).toBe(false); // TODO the response should be a thrown error imo
 
-    const [popup] = await Promise.all([
-      // It is important to call waitForEvent before click to set up waiting.
-      context.waitForEvent('page'), // the background worker opens a new page which is the popup
-      // Opens popup.
-      page.evaluate(() => {
-        window.emeris.enable();
-      }),
-    ]);
-    await popup.click('text=Accept');
+    await enableWebsite(context, page);
 
     // positive test
     expect(
@@ -32,15 +26,15 @@ test.describe('Whitelisting', () => {
     ).not.toBe(false);
 
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html#/whitelisted?browser=true`);
-    await expect(page.locator('text=https://app.emeris.com').first()).toBeVisible();
+    await expect(page.locator('text=https://www.google.com').first()).toBeVisible();
 
     // disconnect page
     await page.click('text=disconnect');
     await page.click('text=Remove');
 
     // check if disconnected
-    await expect(page.locator('text=https://app.emeris.com')).not.toBeVisible();
-    await page.goto(`https://app.emeris.com`);
+    await expect(page.locator('text=https://www.google.com')).not.toBeVisible();
+    await page.goto(`https://www.google.com`);
     expect(
       await page.evaluate(() => {
         return window.emeris.supportedChains();
