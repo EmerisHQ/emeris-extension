de8f31adad45ddef26c6d440dfb9bded4419c9af
Fabian Weber <fabian.weber@tendermint.com>
2022-04-22 12:38:56 +0800

    ignore eslint sort imports rule for yaml

--- src/components/Yaml.vue
+++ src/components/Yaml.vue
@@ -3,14 +3,15 @@
 </template>
 
 <script>
-/* eslint-disable sort-imports */
+/* eslint-disable */
 import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
 import 'prismjs';
+import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-yaml';
 import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
+/* eslint-enable */
 
 import yaml from 'js-yaml';
-import { highlight, languages } from 'prismjs/components/prism-core';
 import { PrismEditor } from 'vue-prism-editor';
 
 export default {
