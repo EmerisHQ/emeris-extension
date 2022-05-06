ca3cf13263b4841a1c0dbf803b1bf480516c7927
Raymahni <busram2009@gmail.com>
2022-05-06 13:03:59 +0100

    adjusted e2e tests

--- e2e/accounts.spec.ts
+++ e2e/accounts.spec.ts
@@ -10,7 +10,7 @@ test.describe('Account Create', () => {
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true`);
     await expect(page.locator('text=Create Account >> visible=true')).toBeVisible();
     await page.click('text=Create Account >> visible=true');
-    await page.fill('[placeholder="Enter a password"]', '123456A$');
+    await page.fill('[placeholder="Enter password"]', '123456A$');
     await page.fill('[placeholder="Confirm password"]', '123456A$');
     await page.click('text=Continue');
     await page.fill('[placeholder="Account Name"]', 'Test Account Created');
@@ -27,7 +27,7 @@ test.describe('Account Create', () => {
     await expect(page.locator('text=Create Account >> visible=true')).toBeVisible();
     await page.click('text=Create Account >> visible=true');
 
-    await page.fill('[placeholder="Enter a password"]', '123456A$');
+    await page.fill('[placeholder="Enter password"]', '123456A$');
     await page.fill('[placeholder="Confirm password"]', '123456A$');
     await page.click('text=Continue');
 
@@ -110,41 +110,36 @@ test.describe('Account Create', () => {
 
   test('Switch account', async ({ page, context }) => {
     async function waitForEvent(page, eventName, seconds) {
-
       seconds = seconds || 30;
 
       // use race to implement a timeout
       return Promise.race([
+        // add event listener and wait for event to fire before returning
+        page.evaluate(function (eventName) {
+          return new Promise(function (resolve) {
+            document.addEventListener(eventName, function () {
+              resolve(); // resolves when the event fires
+            });
+          });
+        }, eventName),
 
-          // add event listener and wait for event to fire before returning
-          page.evaluate(function(eventName) {
-              return new Promise(function(resolve, reject) {
-                  document.addEventListener(eventName, function(e) {
-                      resolve(); // resolves when the event fires
-                  });
-              });
-          }, eventName),
-
-          // if the event does not fire within n seconds, exit
-          page.waitForTimeout(seconds * 1000)
+        // if the event does not fire within n seconds, exit
+        page.waitForTimeout(seconds * 1000),
       ]);
     }
-      
+
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true`);
     await importAccount(page);
-    await page.waitForTimeout(1 * 1000)
+    await page.waitForTimeout(1 * 1000);
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true#/accountAddAdditional`);
     await importAccount(page, 'Test Import Account 2');
-    await page.waitForTimeout(1 * 1000)
+    await page.waitForTimeout(1 * 1000);
     await page.goto(`chrome-extension://${process.env.EXTENSION_ID}/popup.html?browser=true#/accounts`);
 
-    const secondPage = await context.newPage()
-    await secondPage.goto('https://www.google.com')
+    const secondPage = await context.newPage();
+    await secondPage.goto('https://www.google.com');
 
     // on changing the account see if the window receives the event
-    await Promise.all([
-      waitForEvent(secondPage, 'emeris_account_changed', 3),
-      page.click('text=Test Import Account')
-    ])
-  })
+    await Promise.all([waitForEvent(secondPage, 'emeris_account_changed', 3), page.click('text=Test Import Account')]);
+  });
 });
