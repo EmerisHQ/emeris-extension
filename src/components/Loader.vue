9665a6fe11ed8efd891cbe64fbc09d32f3e9679e
Dawntraoz <albasilventefu@gmail.com>
2022-05-11 13:50:19 +0200

    fix: smaller size

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -2,7 +2,7 @@
   <div class="relative h-full bg-bg flex flex-col items-center justify-center">
     <div class="flex items-center justify-center mb-5">
       <EphemerisSpinner class="mx-auto h-20 w-20" />
-      <img class="absolute w-5 h-5 mx-auto" src="/images/GradientCircle.svg" />
+      <img class="absolute w-[18px] h-[18px] mx-auto" src="/images/GradientCircle.svg" />
     </div>
     <p class="text-center w-full secondary-text">Please wait</p>
   </div>
