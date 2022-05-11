a3cb073cdc651d1472a4fec48bcb1add5401df95
Dawntraoz <albasilventefu@gmail.com>
2022-05-11 14:52:38 +0200

    feat: add proper SVGs and tailwind animation

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="relative h-full bg-bg flex flex-col items-center justify-center">
-    <div class="flex items-center justify-center mb-5">
-      <EphemerisSpinner class="mx-auto h-20 w-20" />
+    <div class="relative h-24 w-24 flex items-center justify-center">
+      <img
+        class="w-full absolute rotate-[143deg] animate-golden-ring-1"
+        src="/images/GoldenRingUp.svg"
+        alt="Gold ephemeris ring up"
+      />
+      <img
+        class="w-full absolute rotate-[150deg] animate-golden-ring-2"
+        src="/images/GoldenRingDown.svg"
+        alt="Gold ephemeris ring down"
+      />
       <img class="absolute w-[18px] h-[18px] mx-auto" src="/images/GradientCircle.svg" />
     </div>
-    <p class="text-center w-full secondary-text">Please wait</p>
+    <p class="text-center w-full secondary-text mt-3">Please wait</p>
   </div>
 </template>
-
-<script lang="ts" setup>
-import EphemerisSpinner from '@/components/ui/EphemerisSpinner.vue';
-</script>
