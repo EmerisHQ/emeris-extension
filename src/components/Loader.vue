20f3a2df10cae6f8eb9431eaa7fd22216a2e52ca
Dawntraoz <albasilventefu@gmail.com>
2022-05-11 16:03:32 +0200

    test: add the selector class missing

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative h-full flex flex-col items-center justify-center">
-    <div class="relative h-24 w-24 flex items-center justify-center">
+    <div class="ephemeris relative h-24 w-24 flex items-center justify-center">
       <img
         class="w-full absolute rotate-[143deg] animate-golden-ring-1"
         src="/images/GoldenRingUp.svg"
