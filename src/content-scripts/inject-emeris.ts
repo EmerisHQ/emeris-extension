7dac9f41c88d3045283b07d221d887e3c8672a6e
fl-y <thedigitalimg@gmail.com>
2022-05-13 00:26:14 +0900

    fix: remove old testing code

--- src/content-scripts/inject-emeris.ts
+++ src/content-scripts/inject-emeris.ts
@@ -1,10 +1,6 @@
-import browser from 'webextension-polyfill';
-
 import { ProxyEmeris } from '@@/lib/ProxyEmeris';
 
 import { init } from './init';
-
-console.log(browser);
 
 const emeris = new ProxyEmeris();
 init(emeris);
