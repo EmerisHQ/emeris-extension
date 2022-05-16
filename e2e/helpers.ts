46cdce5a3710c1f7c5756a4773cfcf6410e9fab2
Raymahni <49952972+Raymahni@users.noreply.github.com>
2022-05-16 17:13:24 +0100

    fix: worked on UIX manage password - issue 103 (#109)
    Co-authored-by: Jesse Eilers <eitjuh@gmail.com>
    Co-authored-by: Jesse <jesse.eilers@tendermint.com>

--- e2e/helpers.ts
+++ e2e/helpers.ts
@@ -9,8 +9,8 @@ export const importAccount = async (page, name = 'Test Account Imported') => {
   await expect(page.locator('text=Import account >> visible=true')).toBeVisible();
   await page.click('text=Import Account >> visible=true');
 
-  if (await page.$('[placeholder="Enter a password"]')) {
-    await page.fill('[placeholder="Enter a password"]', '123456A$');
+  if (await page.$('[placeholder="Enter password"]')) {
+    await page.fill('[placeholder="Enter password"]', '123456A$');
     await page.fill('[placeholder="Confirm password"]', '123456A$');
     await page.click('text=Continue');
   }
@@ -52,8 +52,8 @@ export const enableWebsite = async (context, page, withNetwork = false) => {
     }, withNetwork),
   ]);
 
-  await expect(popup.locator('[placeholder="Enter a password"] >> visible=true')).toBeVisible();
-  await popup.fill('[placeholder="Enter a password"]', '123456A$');
+  await expect(popup.locator('[placeholder="Enter password"] >> visible=true')).toBeVisible();
+  await popup.fill('[placeholder="Enter password"]', '123456A$');
   await popup.fill('[placeholder="Confirm password"]', '123456A$');
   await popup.click('text=Continue');
 
