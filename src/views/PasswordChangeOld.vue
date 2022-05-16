897cb3b225c41fab5c1301c3055b04f437acb8ce
Raymahni <busram2009@gmail.com>
2022-05-16 13:36:47 +0100

    disabled submit button when veryfying password on password old change page

--- src/views/PasswordChangeOld.vue
+++ src/views/PasswordChangeOld.vue
@@ -2,19 +2,13 @@
   <div class="page">
     <Header title="Current password" />
     <div class="form" @keyup.enter="submit">
-      <span class="secondary-text" style="margin-top: 16px; margin-bottom: 24px"
-        >Enter your current password to proceed.</span
-      >
-      <div style="margin-bottom: 16px">
+      <span class="secondary-text mt-2 mb-4">Enter your current password to proceed.</span>
+      <div class="mb-4">
         <Input v-model="password" placeholder="Enter current password" type="password" />
         <span v-if="error" class="form-info error">The password is wrong</span>
       </div>
-      <div
-        :style="{
-          marginTop: 'auto',
-        }"
-      >
-        <Button type="submit" name="Continue" @click="submit" />
+      <div class="mt-auto">
+        <Button type="submit" name="Continue" :disabled="btnDisabled" @click="submit" />
       </div>
     </div>
   </div>
@@ -34,14 +28,19 @@ export default defineComponent({
   data: () => ({
     password: '',
     error: false,
+    btnDisabled: false,
   }),
   methods: {
     async submit() {
       this.error = false;
+      this.btnDisabled = true;
+
       const wallet = await this.$store.dispatch(GlobalEmerisActionTypes.UNLOCK_WALLET, { password: this.password });
       if (wallet) {
+        this.btnDisabled = false;
         this.$router.push('/passwordChange/new');
       } else {
+        this.btnDisabled = false;
         this.error = true;
       }
     },
