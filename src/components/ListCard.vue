71a3ed8759d9c161edeadd924c14641eacceb38f
Dawntraoz <albasilventefu@gmail.com>
2022-05-17 13:51:52 +0200

    fix: leading 5 per design

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="list-card-container flex items-center justify-center self-stretch flex-none grow-0 order-none text-[13px] py-5 px-6 mb-3 rounded-[10px]"
+    class="list-card-container flex items-center justify-center self-stretch flex-none grow-0 order-none text-[13px] leading-5 py-5 px-6 mb-3 rounded-[10px]"
   >
     <img :src="img" class="mr-4 w-8" />
     <span>{{ caption }}</span>
