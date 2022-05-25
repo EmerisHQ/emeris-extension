dc170bc90c337a76a27b38108c663acbe972a95f
Dawntraoz <albasilventefu@gmail.com>
2022-05-25 16:00:46 +0200

    feat: receive assets views refactor script and fix routing

--- src/views/ReceiveQR.vue
+++ src/views/ReceiveQR.vue
@@ -1,24 +1,18 @@
 <template>
   <Loader v-if="!asset" />
   <template v-else>
-    <Header :title="`Receive ${asset.display_name}`" />
-    <div style="text-align: center; margin-top: -24px">
-      <span class="secondary-text" style="font-size: 13px">on {{ asset.on_chain }}</span>
+    <Header :title="`Receive ${asset.display_name}`" back-to="/receive" />
+    <div class="text-center -mt-6">
+      <span class="secondary-text text-[13px]">on {{ asset.on_chain }}</span>
     </div>
     <template v-if="recipientAddress">
-      <QrCode
-        class="relative z-10"
-        :value="recipientAddress"
-        width="160"
-        color="FFFFFF"
-        style="margin-left: auto; margin-right: auto; margin-top: 79px; margin-bottom: 73px"
-      />
-      <div style="text-align: left; display: flex; flex-direction: column">
+      <QrCode class="relative z-10 mx-auto mt-20 mb-[73px]" :value="recipientAddress" width="160" color="FFFFFF" />
+      <div class="text-left flex flex-col">
         <span>Address</span>
-        <span class="secondary-text" style="margin-bottom: 24px; word-wrap: break-word">{{ recipientAddress }}</span>
-        <div style="color: #4ef2e4; cursor: pointer; display: flex" @click="pasteClip">
-          <Icon v-if="!copied" name="CopyIcon" :icon-size="1" style="margin-right: 12px" />
-          <span v-else style="margin-right: 12px">✓</span>
+        <span class="secondary-text mb-6 break-words">{{ recipientAddress }}</span>
+        <div class="cursor-pointer flex text-quaternary" @click="pasteClip">
+          <Icon v-if="!copied" name="CopyIcon" :icon-size="1" class="mr-3" />
+          <span v-else class="mr-3">✓</span>
           Copy to clipboard
         </div>
       </div>
@@ -26,7 +20,7 @@
   </template>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { computed, ref, watch } from '@vue/runtime-core';
 import orderBy from 'lodash.orderby';
 import { useStore } from 'vuex';
@@ -40,67 +34,56 @@ import Header from '@@/components/Header.vue';
 import Loader from '@@/components/Loader.vue';
 import { GlobalEmerisActionTypes } from '@@/store/extension/action-types';
 
-export default {
-  name: 'Receive QR',
-  components: {
-    Header,
-    QrCode,
-    Icon,
-    Loader,
-  },
-  props: {
-    denom: { type: String, required: true },
-  },
-  setup(props) {
-    const { nativeBalances } = useAccount();
-    const store = useStore();
-    let displayNameAddedList = ref([]);
-    let recipientAddress = ref('');
-    let copied = ref(false);
+const store = useStore();
+const { nativeBalances } = useAccount();
 
-    const assetsList = computed(() => {
-      if (!store.getters[GlobalGetterTypes.API.getVerifiedDenoms]) return [];
-      return orderBy(nativeBalances.value, (item) => (item.base_denom.startsWith('pool') ? 1 : -1));
-    });
+const props = defineProps<{
+  denom: string;
+}>();
 
-    const asset = computed(() => {
-      return displayNameAddedList.value.find((asset) => asset.base_denom === props.denom);
-    });
+let displayNameAddedList = ref([]);
+let recipientAddress = ref('');
+let copied = ref(false);
 
-    const pasteClip = () => {
-      navigator.clipboard.writeText(recipientAddress.value);
+const assetsList = computed(() => {
+  if (!store.getters[GlobalGetterTypes.API.getVerifiedDenoms]) return [];
+  return orderBy(nativeBalances.value, (item) => (item.base_denom.startsWith('pool') ? 1 : -1));
+});
 
-      if (copied.value) clearTimeout(copied.value);
-      copied.value = setTimeout(() => (copied.value = false), 3000);
-    };
+const asset = computed(() => {
+  return displayNameAddedList.value.find((asset) => asset.base_denom === props.denom);
+});
 
-    watch(
-      () => assetsList.value,
-      async (value) => {
-        displayNameAddedList.value = await Promise.all(
-          value.map(async (asset) => {
-            return {
-              ...asset,
-              display_name: await getDisplayName(asset.base_denom, store.getters[GlobalGetterTypes.API.getDexChain]),
-            };
-          }),
-        );
-      },
-      { immediate: true },
-    );
+const pasteClip = () => {
+  navigator.clipboard.writeText(recipientAddress.value);
 
-    watch(
-      () => asset.value,
-      async (assetValue) => {
-        if (!assetValue) return;
-        recipientAddress.value = await store.dispatch(GlobalEmerisActionTypes.GET_ADDRESS, {
-          chainId: assetValue.on_chain,
-        });
-      },
-      { immediate: true },
-    );
-
-    return { assetsList, pasteClip, asset, recipientAddress, copied };
-  },
+  if (copied.value) clearTimeout(copied.value);
+  copied.value = setTimeout(() => (copied.value = false), 3000);
 };
+
+watch(
+  () => assetsList.value,
+  async (value) => {
+    displayNameAddedList.value = await Promise.all(
+      value.map(async (asset) => {
+        return {
+          ...asset,
+          display_name: await getDisplayName(asset.base_denom, store.getters[GlobalGetterTypes.API.getDexChain]),
+        };
+      }),
+    );
+  },
+  { immediate: true },
+);
+
+watch(
+  () => asset.value,
+  async (assetValue) => {
+    if (!assetValue) return;
+    recipientAddress.value = await store.dispatch(GlobalEmerisActionTypes.GET_ADDRESS, {
+      chainId: assetValue.on_chain,
+    });
+  },
+  { immediate: true },
+);
 </script>
