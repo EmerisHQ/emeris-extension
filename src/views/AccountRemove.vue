cf04670d6ede7b7d22f8c4718dba0ce590f0ac2c
Raymahni <busram2009@gmail.com>
2022-05-25 16:52:33 +0100

    worked on issue 81

--- src/views/AccountRemove.vue
+++ src/views/AccountRemove.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="page">
     <Header title="Remove account" :back-to="$route.query.backto" />
-    <div class="mt-32">
-      <Icon name="InformationIcon" :icon-size="1.5" class="mb-4" />
-      <p class="font-medium text-2 text-center">Are you sure you want to remove {{ account.accountName }} account?</p>
+    <div v-if="!backedUp(account)" class="text-center mt-10">
+      <Icon name="WarningTriangleIcon" :icon-size="2" class="text-warning mb-4" />
+      <p class="font-medium text-2 mb-4">This account is not backed up</p>
+      <p class="secondary-text">
+        If you haven’t backed up your secret recovery phrase, you may lose assets associated with this account. Are you
+        sure you want to remove it?
+      </p>
+    </div>
+    <div v-else class="text-center mt-32">
+      <Icon name="InformationIcon" :icon-size="2" class="mb-4" />
+      <p class="font-medium text-2">Are you sure you want to remove {{ account.accountName }} account?</p>
     </div>
     <div class="mt-auto">
-      <Button class="reset-btn" name="Remove account" @click="removeWallet" />
-      <Button name="Cancel" variant="link" />
+      <Button class="reset-btn mb-4" name="Remove account" @click="removeWallet" />
+      <Button v-if="!backedUp(account)" class="mb-4" variant="secondary" name="Back up account" @click="backUpWallet" />
+      <Button name="Cancel" variant="link" @click="$router.push(`/account-settings/${index}`)" />
     </div>
   </div>
 </template>
@@ -21,6 +30,7 @@ import Button from '@/components/ui/Button.vue';
 import Icon from '@/components/ui/Icon.vue';
 import Header from '@@/components/Header.vue';
 import { GlobalEmerisActionTypes } from '@@/store/extension/action-types';
+import { AccountCreateStates } from '@@/types';
 
 const store = useStore();
 const router = useRouter();
@@ -39,9 +49,17 @@ const account = computed(() => {
   return wallet.value[props.index];
 });
 
+const backedUp = (account) => {
+  return account.setupState === AccountCreateStates.COMPLETE;
+};
+
 const removeWallet = async () => {
   await store.dispatch(GlobalEmerisActionTypes.REMOVE_ACCOUNT, { accountName: account.value.accountName });
   router.push('/accounts');
+};
+
+const backUpWallet = () => {
+  router.push('/backup');
 };
 </script>
 
