7c49340c0bcdc88fdd05b3a7788ab1c4282d16b0
Raymahni <busram2009@gmail.com>
2022-05-25 14:58:09 +0100

    started work on issue 81

--- src/views/AccountRemove.vue
+++ src/views/AccountRemove.vue
@@ -1,57 +1,52 @@
 <template>
-  <Loader v-if="!account" />
-  <ConfirmationScreen
-    v-else
-    :title="`Are you sure that you want to remove ${account.accountName}?`"
-    subtitle="If you have not backed up this wallet, you will lose access entirely"
-  >
-    <div
-      :style="{
-        marginTop: 'auto',
-      }"
-      class="buttons"
-    >
-      <Button name="Remove" @click="removeWallet" />
-      <router-link to="/accounts">
-        <Button name="Cancel" variant="link" />
-      </router-link>
+  <div class="page">
+    <Header title="Remove account" :back-to="$route.query.backto" />
+    <div class="mt-32">
+      <Icon name="InformationIcon" :icon-size="1.5" class="mb-4" />
+      <p class="font-medium text-2 text-center">Are you sure you want to remove {{ account.accountName }} account?</p>
     </div>
-  </ConfirmationScreen>
+    <div class="mt-auto">
+      <Button class="reset-btn" name="Remove account" @click="removeWallet" />
+      <Button name="Cancel" variant="link" />
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { mapState } from 'vuex';
+<script lang="ts" setup>
+import { computed } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
 import Button from '@/components/ui/Button.vue';
-import Loader from '@@/components/Loader.vue';
-import { RootState } from '@@/store';
+import Icon from '@/components/ui/Icon.vue';
+import Header from '@@/components/Header.vue';
 import { GlobalEmerisActionTypes } from '@@/store/extension/action-types';
-import ConfirmationScreen from '@@/views/ConfirmationScreen.vue';
 
-export default defineComponent({
-  name: 'Account Remove',
-  computed: {
-    ...mapState({
-      wallet: (state: RootState) => state.extension.wallet,
-    }),
-    account() {
-      return this.wallet[this.index];
-    },
-  },
-  components: {
-    Button,
-    ConfirmationScreen,
-    Loader,
-  },
-  props: {
-    index: { type: Number, required: true },
-  },
-  methods: {
-    async removeWallet() {
-      await this.$store.dispatch(GlobalEmerisActionTypes.REMOVE_ACCOUNT, { accountName: this.account.accountName });
-      this.$router.push('/accounts');
-    },
-  },
+const store = useStore();
+const router = useRouter();
+
+interface Props {
+  index: number;
+}
+
+const props = withDefaults(defineProps<Props>(), { index: 0 });
+
+const wallet = computed(() => {
+  return store.state.extension.wallet;
 });
+
+const account = computed(() => {
+  return wallet.value[props.index];
+});
+
+const removeWallet = async () => {
+  await store.dispatch(GlobalEmerisActionTypes.REMOVE_ACCOUNT, { accountName: account.value.accountName });
+  router.push('/accounts');
+};
 </script>
+
+<style lang="scss" scoped>
+:deep(.reset-btn button) {
+  background: #ff3d56 !important;
+}
+</style>
