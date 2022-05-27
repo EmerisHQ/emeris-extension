374df22e8a10ea69700a463598b9e8fb5c3c0444
Dawntraoz <albasilventefu@gmail.com>
2022-05-27 10:05:54 +0200

    fix: extension reset confirm redirect and setup

--- src/views/ExtensionResetConfirm.vue
+++ src/views/ExtensionResetConfirm.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="page">
-    <Header title="Forgot password" />
+    <Header title="Forgot password" back-to="/extensionReset" />
     <div class="mt-20">
-      <Icon name="InformationIcon" icon-size="2" class="mb-1.5" />
+      <Icon name="InformationIcon" :icon-size="2" class="mb-1.5" />
       <p class="font-medium text-2 text-center">Are you sure you want to reset your wallet?</p>
       <p class="secondary-text mt-4 mb-1.5 text-center">
         To avoid permanently losing access to your assets, ensure you have backed up the secret recovery phrases for all
@@ -18,31 +18,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { useStore } from 'vuex';
 
 import Button from '@/components/ui/Button.vue';
 import Icon from '@/components/ui/Icon.vue';
 import Header from '@@/components/Header.vue';
 import { GlobalEmerisActionTypes } from '@@/store/extension/action-types';
 
-export default defineComponent({
-  name: 'Extension Reset Confirm',
-  components: {
-    Button,
-    Icon,
-    Header,
-  },
-  methods: {
-    async submit() {
-      await this.$store.dispatch(GlobalEmerisActionTypes.EXTENSION_RESET);
-      this.$router.push('/extensionReset/confirmed');
-    },
-  },
-});
+const store = useStore();
+const router = useRouter();
+
+const submit = async () => {
+  await store.dispatch(GlobalEmerisActionTypes.EXTENSION_RESET);
+  router.push('/extensionReset/confirmed');
+};
 </script>
+
 <style lang="scss" scoped>
 :deep(.reset-btn button) {
-  background: #ff3d56 !important;
+  @apply bg-negative;
 }
 </style>
