9a2324236bf429056193cf4893290357cae28b5b
Jesse Eilers <eitjuh@gmail.com>
2022-05-30 11:45:25 +0200

    fix/custom-tx-in-preview-too-small

--- src/components/Transactions/custom.vue
+++ src/components/Transactions/custom.vue
@@ -1,58 +1,39 @@
 <template>
-  <div style="text-align: left">
-    <span style="margin-bottom: 12px; display: block">Custom transaction</span>
-
+  <div class="text-left">
+    <span class="mb-3 block">Custom transaction</span>
     <div
-      style="display: flex; flex-direction: column; overflow: hidden; transition: max-height 0.5s ease-out"
-      :style="{
-        height: expand ? $refs.innerSlideout.clientHeight + 'px' : '36px',
-      }"
+      class="flex flex-col overflow-hidden transition-height duration-500 ease-out"
+      :class="{ 'h-max': expand, 'h-32': !expand }"
     >
-      <Yaml :json="message.raw" />
+      <Yaml :json="message" />
     </div>
     <div
       ref="innerSlideout"
-      style="
-        display: flex;
-        flex-direction: column;
-        vertical-align: center;
-        border-top: 1px solid #ffffff24;
-        width: calc(100% + 32px);
-        margin-left: -16px;
-      "
+      class="flex flex-col align-middle ml-4 border-t-1 border-white/[.14]"
+      style="border-top: 1px solid #ffffff24"
       @click="expand = !expand"
     >
       <Icon
         :name="'CaretDownIcon'"
         :icon-size="1"
-        class="transform transition-transform -ml-2"
+        class="transform transition-transform -ml-2 mt-4"
         :class="{ 'rotate-180': expand }"
-        style="margin-top: 16px"
       />
     </div>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import { EmerisTransactions } from '@emeris/types';
+import { ref } from 'vue';
 
 import Icon from '@/components/ui/Icon.vue';
 
 import Yaml from '../Yaml.vue';
-export default {
-  components: {
-    Yaml,
-    Icon,
-  },
-  props: {
-    message: { type: EmerisTransactions.CustomData, required: true },
-  },
-  data: () => ({
-    expand: false,
-  }),
 
-  Yaml,
-};
+defineProps<{
+  message: EmerisTransactions.CustomData;
+}>();
+
+let expand = ref(false);
 </script>
-
-<style></style>
