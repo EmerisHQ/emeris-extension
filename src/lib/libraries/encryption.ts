758812686ff6eb7f1e258f99f81905a54af5e6da
Fabian <frznhope@gmail.com>
2022-06-01 13:47:49 +0300

    feat: password as web security (#190)
    * remove change password functionality
    
    * import password as webcrypto to store
    
    * only store secure key
    
    * store key as jwk
    
    * add salt to build pipeline
    
    * added salt to testing
    
    * Update src/lib/EmerisStorage.ts
    
    Co-authored-by: Alba Silvente Fuentes <36744484+Dawntraoz@users.noreply.github.com>
    
    * reduce workers
    
    * replace google with emeris homepage
    
    * fix decryption
    
    * fixed issues with whitelisting tests
    
    Co-authored-by: Fabian Weber <fabian.weber@tendermint.com>
    Co-authored-by: Alba Silvente Fuentes <36744484+Dawntraoz@users.noreply.github.com>

--- src/lib/libraries/encryption.ts
+++ src/lib/libraries/encryption.ts
@@ -1,7 +1,7 @@
 import scrypt from 'scrypt-js';
 import buffer from 'scrypt-js/thirdparty/buffer';
 
-const getSecureKey = (passwordInput) => {
+const getSecureKey = (passwordInput: string) => {
   const password = new buffer.SlowBuffer(passwordInput.normalize('NFKC'));
   const salt = new buffer.SlowBuffer(process.env.VITE_ENCRYPTION_SALT.normalize('NFKC'));
 
@@ -15,34 +15,43 @@ const getSecureKey = (passwordInput) => {
   return key;
 };
 
-export const encrypt = async (value, password) => {
-  const key = getSecureKey(password);
-  const iv = Buffer.from(process.env.VITE_IV);
-  const key_encoded = await crypto.subtle.importKey('raw', key.buffer, 'AES-GCM', false, ['encrypt', 'decrypt']);
-  const encrypted = await crypto.subtle.encrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv,
-    },
-    key_encoded,
-    Buffer.from(value),
-  );
-  return Buffer.from(encrypted).toString('hex');
+export const importKey = async (passwordInput: string) => {
+  const secureKey = getSecureKey(passwordInput);
+  const importedKey = await crypto.subtle.importKey('raw', secureKey.buffer, 'AES-GCM', true, ['encrypt', 'decrypt']);
+  return importedKey;
 };
 
-export const decrypt = async (value, password) => {
-  const key = getSecureKey(password);
-  const iv = Buffer.from(process.env.VITE_IV);
-  const key_encoded = await crypto.subtle.importKey('raw', key.buffer, 'AES-GCM', false, ['encrypt', 'decrypt']);
-  const decrypted = await crypto.subtle.decrypt(
-    {
-      name: 'AES-GCM',
-      iv: iv,
-    },
-    key_encoded,
-    Buffer.from(value, 'hex'),
-  );
-  const decoder = new TextDecoder();
-  const plaintext = decoder.decode(decrypted);
-  return plaintext;
+export const encrypt = async (plaintext, importedKey: CryptoKey) => {
+  const iv = crypto.getRandomValues(new Uint8Array(12)); // get 96-bit random iv
+  const ivStr = Array.from(iv)
+    .map((b) => String.fromCharCode(b))
+    .join(''); // iv as utf-8 string
+
+  const alg = { name: 'AES-GCM', iv: iv }; // specify algorithm to use
+
+  const ptUint8 = new TextEncoder().encode(plaintext); // encode plaintext as UTF-8
+  const ctBuffer = await crypto.subtle.encrypt(alg, importedKey, ptUint8); // encrypt plaintext using key
+
+  const ctArray = Array.from(new Uint8Array(ctBuffer)); // ciphertext as byte array
+  const ctStr = ctArray.map((byte) => String.fromCharCode(byte)).join(''); // ciphertext as string
+
+  return Buffer.from(ivStr + ctStr).toString('base64');
+};
+
+export const decrypt = async (ciphertext, importedKey: CryptoKey) => {
+  const ivStr = Buffer.from(ciphertext, 'base64').toString().slice(0, 12); // decode base64 iv
+  const iv = new Uint8Array(Array.from(ivStr).map((ch) => ch.charCodeAt(0))); // iv as Uint8Array
+
+  const alg = { name: 'AES-GCM', iv: iv }; // specify algorithm to use
+
+  const ctStr = Buffer.from(ciphertext, 'base64').toString().slice(12); // decode base64 ciphertext
+  const ctUint8 = new Uint8Array(Array.from(ctStr).map((ch) => ch.charCodeAt(0))); // ciphertext as Uint8Array
+
+  try {
+    const plainBuffer = await crypto.subtle.decrypt(alg, importedKey, ctUint8); // decrypt ciphertext using key
+    const plaintext = new TextDecoder().decode(plainBuffer); // plaintext from ArrayBuffer
+    return plaintext; // return the plaintext
+  } catch (e) {
+    throw new Error('Decrypt failed');
+  }
 };
